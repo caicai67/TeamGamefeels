66aa6cf71f1bb2bf4de69c4df8ac4934aae64256
chrisdonlan87 <chris.donlan87@gmail.com>
2016-06-15 10:23:52 -0400

    Added Camera Material
    PS4 input is not set up.  I'll have to redo it, and see if the merge
    keeps the info

---
+++ Milestone2/Assets/Character/Camera/AbstractTargetFollower.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class AbstractTargetFollower : MonoBehaviour {
+	public enum UpdateType
+	{
+		FixedUpdate,
+		LateUpdate,
+		ManualUpdate,
+	}
+	[SerializeField] protected Transform target;
+	[SerializeField] private bool autoTargetPlayer = true;
+	[SerializeField] private UpdateType updateType;
+
+	protected Rigidbody targetRigidbody;
+
+	protected virtual void Start()
+	{
+		// if auto targeting is used, find the object tagged "Player"
+		// any class inheriting from this should call base.Start() to perform this action!
+		if (this.autoTargetPlayer) {
+			FindAndTargetPlayer ();
+		}
+	}
+
+	private void FixedUpdate()
+	{
+		// we update from here if updatetype is set to Fixed, or in auto mode,
+		// if the target has a rigidbody, and isn't kinematic.
+		if (this.autoTargetPlayer && (this.target == null || !this.target.gameObject.activeSelf))
+		{
+			FindAndTargetPlayer();
+		}
+		if (this.updateType == UpdateType.FixedUpdate)
+		{
+			FollowTarget(Time.deltaTime);
+		}
+	}
+
+	public void LateUpdate()
+	{
+		if (this.autoTargetPlayer && (this.target == null || !this.target.gameObject.activeSelf)){
+			FindAndTargetPlayer ();
+		}
+		if (this.updateType == UpdateType.LateUpdate) {
+			FollowTarget (Time.deltaTime);
+		}
+	}
+	public void ManualUpdate()
+	{
+		// we update from here if updatetype is set to Late, or in auto mode,
+		// if the target does not have a rigidbody, or - does have a rigidbody but is set to kinematic.
+		if (this.autoTargetPlayer && (this.target == null || !this.target.gameObject.activeSelf)) {
+			FindAndTargetPlayer ();
+		}
+		if (this.updateType == UpdateType.ManualUpdate) {
+			FollowTarget (Time.deltaTime);
+		}
+	}
+	protected abstract void FollowTarget (float deltaTime);
+
+	public void FindAndTargetPlayer()
+	{
+		var targetObj = GameObject.FindGameObjectWithTag ("Player");
+		if (targetObj) {
+			SetTarget (targetObj.transform);
+		}
+	}
+
+	public virtual void SetTarget(Transform newTransform)
+	{
+		this.target = newTransform;
+	}
+
+	public Transform Target
+	{
+		get { return this.target; }
+	}
+}
---
+++ Milestone2/Assets/Character/Camera/PivotBasedCameraRig.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class PivotBasedCameraRig : AbstractTargetFollower {
+	// This script is designed to be placed on the root object of a camera rig,
+	// comprising 3 gameobjects, each parented to the next:
+
+	// 	Camera Rig
+	// 		Pivot
+	// 			Camera
+	protected Transform cam; // the transform of the camera
+	protected Transform pivot; // position of the pivot point
+	protected Vector3 lastTargetPosition;
+
+	protected virtual void Awake()
+	{
+		// find the camera in the object hierarchy
+		this.cam = GetComponentInChildren<Camera>().transform;
+		//this.pivot = this.target.transform;
+		this.pivot = this.cam.parent;
+	}
+}
