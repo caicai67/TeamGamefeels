c2fb4d16e2cd9d16d55d7a147ab3d774d958161a
caicai67 <caitlinmorris@Caitlins-MacBook-Pro.local>
2016-06-22 18:25:31 -0400

    moving sidewalk changes

--- Milestone2/Assets/Levels/Gardens/Caitlin Garden/scripts/MovingSidewalkBehavior.cs
+++ Milestone2/Assets/Levels/Gardens/Caitlin Garden/scripts/MovingSidewalkBehavior.cs
@@ -5,14 +5,27 @@ public class MovingSidewalkBehavior : MonoBehaviour {
 
 	public float pallet_Length;
 	public int pallet_Count;
+	public float speed;
 
 	// Use this for initialization
 	void Start () {
-	
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		
+	}
+	void FixedUpdate () {
+		
+	}
+
+	//capture an object when it reaches the moving sidewalk
+	void OnTriggerStay (Collider captured) {
+		captured.gameObject.transform.parent = this.transform.Find("Moving");
+	}
+
+	void OnTriggerExit (Collider captured) {
+		captured.gameObject.transform.parent = null;
 	}
 }
