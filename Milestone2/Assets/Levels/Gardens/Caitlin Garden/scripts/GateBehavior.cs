891ecf148596556bdac8a2232f566dd43b80f80c
caicai67 <caitlinmorris@Caitlins-MacBook-Pro.local>
2016-06-23 02:24:36 -0400

    garden changes

---
+++ Milestone2/Assets/Levels/Gardens/Caitlin Garden/scripts/GateBehavior.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GateBehavior : MonoBehaviour {
+	Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnTriggerEnter() {
+		anim.SetBool ("open", true);
+		Debug.Log ("gate trigger entered");
+	}
+	void OnTriggerExit() {
+		anim.SetBool ("open", false);
+	}
+}
