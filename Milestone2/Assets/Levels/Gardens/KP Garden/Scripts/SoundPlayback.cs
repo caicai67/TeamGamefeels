0ba7c8e72cd868c1b235441d3964317a5944d111
Karan Pratap Singh <kps@gatech.edu>
2016-06-24 15:46:29 -0400

    Added a sound script

---
+++ Milestone2/Assets/Levels/Gardens/KP Garden/Scripts/SoundPlayback.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundPlayback : MonoBehaviour {
+
+	void OnCollisionStay(Collision col) {
+		if() {
+			audio.volume = col.relativeVelocity.magnitude/20;
+			audio.Play();
+		}
+	}
+}
