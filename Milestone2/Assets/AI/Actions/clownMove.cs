567daf0168cd2bec1478291a7e44f649ddcbabd2
caicai67 <caitlinmorris@Theresas-Air.attlocal.net>
2016-07-15 16:48:40 -0400

    made changes to script

--- Milestone2/Assets/AI/Actions/clownMove.cs
+++ Milestone2/Assets/AI/Actions/clownMove.cs
@@ -1,15 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
+using RAIN.Serialization;
+using RAIN.Core;
+using RAIN.Entities.Aspects;
+using System.Collections.Generic;
 
-public class clownMove : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+[RAINSerializableClass]
+public class clownMove : CustomAIElement {
+	public override void Act() 
+	{
+		IList<RAINAspect> asps = AI.Senses.SenseAll ();
+		Debug.Log (asps.Count);
+		//step 1 - get sensory input and change mecanim param if something in sensor
+		/*check the sensors
+		if(sees player) { //move behind
+			find furthest angle to back of player he can reach that is x units away
+			teleport to that point
+		} else { //move randomly
+			pick a point at max distance away from self and move there
+		*/
 	}
 }
