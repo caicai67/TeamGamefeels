7158f1339d112be5f3c42b1a90fe45e469ca1b73
caicai67 <caitlinmorris@Caitlins-MacBook-Pro.local>
2016-07-16 19:49:49 -0400

    added player glow to clown script

--- Milestone2/Assets/AI/Actions/clownMove.cs
+++ Milestone2/Assets/AI/Actions/clownMove.cs
@@ -10,26 +10,33 @@ public class clownMove : CustomAIElement {
 	
 	public override void Act() 
 	{
-		//IList<RAINAspect> asps = AI.Senses.SenseAll ();
-		//Animator anim = AI.Body.GetComponent<Animator> ();
-		//foreach (RAINAspect detected in asps) {
-		Debug.Log ("player: " + AI.WorkingMemory.GetItem<GameObject>("detectedPlayer"));
+		//glowing light to show where spell is being cast
+		Light spellGlow = AI.Body.transform.FindChild("spellCast").GetComponent<Light>();
+
+		//check to see if clown can see player
 		if (AI.WorkingMemory.GetItem<GameObject>("detectedPlayer") != null) {
+			
+			//find point behind player
 			GameObject character = AI.WorkingMemory.GetItem<GameObject> ("detectedPlayer");
 			AI.WorkingMemory.SetItem ("targetSpot", character.transform.position - (5 * character.transform.forward));
-			AI.WorkingMemory.SetItem ("playerInRange", Vector3.Magnitude (AI.Body.transform.position - character.transform.position) < 10f);
+
+			//check if clown is close enough to affect player
+			bool inRange = Vector3.Magnitude (AI.Body.transform.position - character.transform.position) < 8f;
+			AI.WorkingMemory.SetItem ("playerInRange", inRange);
+
+			if (inRange) {
+				//turn on glow and place it on player
+				spellGlow.gameObject.transform.position = character.transform.position;
+				spellGlow.intensity = 1;
+			} else {
+				//turn off glow
+				spellGlow.intensity = 0;
+			}
+
 		} else {
 			AI.WorkingMemory.SetItem ("playerInRange", false);
+			//turn off glow
+			spellGlow.intensity = 0;
 		}
-		//}
-		//:) step 1 - get sensory input
-		//:) 2 - change mecanim param if something in sensor
-		/*check the sensors
-		if(sees player) { //move behind
-			find furthest angle to back of player he can reach that is x units away
-			teleport to that point
-		} else { //move randomly
-			pick a point at max distance away from self and move there
-		*/
 	}
 }
