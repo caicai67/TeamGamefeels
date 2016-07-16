e94595b1cd8f73fdea42d5e616cb9734bb2c7694
caicai67 <caitlinmorris@Caitlins-MacBook-Pro.local>
2016-07-16 13:55:30 -0400

    fixed clown behavior tree

--- Milestone2/Assets/AI/Actions/clownMove.cs
+++ Milestone2/Assets/AI/Actions/clownMove.cs
@@ -10,21 +10,19 @@ public class clownMove : CustomAIElement {
 	
 	public override void Act() 
 	{
-		IList<RAINAspect> asps = AI.Senses.SenseAll ();
-
-		//Debug.Log ("white clown sensed objects: " + asps.Count);
+		//IList<RAINAspect> asps = AI.Senses.SenseAll ();
 		Animator anim = AI.Body.GetComponent<Animator> ();
-		foreach (RAINAspect detected in asps) {
-			GameObject character = detected.Entity.Form;
-			if (character.CompareTag ("Player")) {
+		//foreach (RAINAspect detected in asps) {
+			
+		GameObject character = AI.WorkingMemory.GetItem("detectedPlayer");
+			if (character != null) {
 				AI.WorkingMemory.SetItem ("targetSpot", character.transform.position - (5 * character.transform.forward));
-				AI.WorkingMemory.SetItem ("playerInRange", Vector3.Magnitude (AI.Body.transform.position - character.transform.position) < 10f);
+				AI.WorkingMemory.SetItem ("playerInRange", Vector3.Magnitude (AI.Body.transform.position - character.transform.position) < 5f);
 			} else {
-				AI.WorkingMemory.SetItem ("targetSpot", character.transform.position - (5 * character.transform.forward));
 				AI.WorkingMemory.SetItem ("playerInRange", false);
 			}
 
-		}
+		//}
 		//:) step 1 - get sensory input
 		//:) 2 - change mecanim param if something in sensor
 		/*check the sensors
