db74ac974cc94eda704a39162fcf24f7feda43e5
caicai67 <caitlinmorris@Caitlins-MacBook-Pro.local>
2016-07-22 14:29:53 -0400

    Made portal transfer player to new scene
    made portal a prefab and added a script to send the player to “temp2”

---
+++ Milestone2/Assets/Scripts/PortalSend.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class PortalSend : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnTriggerEnter(Collider other) {
+		if (other.CompareTag("Player")){
+			SceneManager.LoadScene ("temp2");
+		}
+	}
+}
