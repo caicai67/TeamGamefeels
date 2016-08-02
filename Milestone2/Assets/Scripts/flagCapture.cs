8301c0888356e8eaed2fce8bd8064c7487d63bb2
caicai67 <caitlinmorris@Caitlins-MacBook-Pro.local>
2016-08-02 13:30:02 -0400

    edited script to change tower mesh

--- Milestone2/Assets/Scripts/flagCapture.cs
+++ Milestone2/Assets/Scripts/flagCapture.cs
@@ -3,13 +3,28 @@ using System.Collections;
 
 public class flagCapture : MonoBehaviour {
 
+	public GameObject closedTower;
+	public GameObject openedTower;
+
 	// Use this for initialization
 	void Start () {
-	
+		closedTower.SetActive (true);
+		openedTower.SetActive (false);
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+	}
+	void OnCollisionEnter(Collision collision) {
+		Debug.Log ("Collided");
+		if (collision.gameObject.CompareTag("Player")){
+			openTower();
+		}
+	}
+	void openTower(){
+		openedTower.SetActive (true);
+		closedTower.SetActive (false);
 	}
 }
