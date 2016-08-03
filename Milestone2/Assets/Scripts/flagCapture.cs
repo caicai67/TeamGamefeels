40d802569f7265efdbbc7da0a0113f5b8ff27134
caicai67 <caitlinmorris@Caitlins-MacBook-Pro.local>
2016-08-03 01:02:19 -0400

    Merge remote-tracking branch 'origin/final' into final
    # Conflicts:
    #	Milestone2/Assets/RAIN/Editor/RAINProjectState.asset
    #	Milestone2/Assets/Scenes/temp2.unity

--- Milestone2/Assets/Scripts/flagCapture.cs
+++ Milestone2/Assets/Scripts/flagCapture.cs
@@ -18,7 +18,6 @@ public class flagCapture : MonoBehaviour {
 	
 	}
 	void OnCollisionEnter(Collision collision) {
-		Debug.Log ("Collided");
 		if (collision.gameObject.CompareTag("Player")){
 			openTower();
 		}
