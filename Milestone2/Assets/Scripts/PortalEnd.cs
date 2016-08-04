fdad75fe7d82d667e745004fb337ffd103d3281b
caicai67 <caitlinmorris@Caitlins-MacBook-Pro.local>
2016-08-04 03:00:36 -0400

    added ending animation(may not work yet)

---
+++ Milestone2/Assets/Scripts/PortalEnd.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class PortalEnd : MonoBehaviour {
+
+	public Canvas canvas;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+	void OnTriggerEnter(Collider other) {
+		if (other.CompareTag("Player")){
+			Animation anim = canvas.GetComponent<Animation> ();
+			anim.Play ();
+		}
+	}
+}
