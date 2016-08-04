d2863667968e2a46398602a360730f209e5fa7a0
caicai67 <caitlinmorris@Caitlins-MacBook-Pro.local>
2016-08-03 20:06:27 -0400

    added script to play cutscene at beginning

---
+++ Milestone2/Assets/Scripts/SceneEntry.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneEntry : MonoBehaviour {
+	public MovieTexture cutscene;
+
+	void Awake() {
+		if (cutscene != null) {
+			Debug.Log ("cutscene playing");
+			cutscene.Play ();
+		}
+	}
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnGUI() {
+		if (cutscene != null && cutscene.isPlaying)
+		{
+			GUI.DrawTexture (new Rect (0, 0, Screen.width, Screen.height), cutscene);
+		}
+	}
+}
