1040fe42fd60abad78f57ec50e75edc41095ea94
chrisdonlan87 <chris.donlan87@gmail.com>
2016-08-04 15:53:19 -0400

    implemented sword combat damage

---
+++ Milestone2/Assets/Character/Controller/CharacterHealth.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterHealth : MonoBehaviour {
+
+	private Animator animator;
+	private Rigidbody rigid_body;
+	private CharacterController controller;
+	public int health = 100;
+	void Awake(){
+		this.animator = GetComponent<Animator> ();
+		this.rigid_body = GetComponent<Rigidbody> ();
+		this.controller = GetComponent<CharacterController> ();
+	}
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (this.health <= 0) {
+			Die ();
+		}
+
+	}
+	public void Die(){
+		this.rigid_body.isKinematic = true;
+		makeRagdollSolid ();
+		this.animator.enabled = false;
+		this.controller.enabled = false;
+	}
+	public void SlashDamage(){
+		this.health -= 100;
+	}
+
+	void makeRagdollSolid() {
+		foreach (CapsuleCollider capsule_collider in GetComponentsInChildren<CapsuleCollider>()) {
+			capsule_collider.isTrigger = false;
+		}
+	}
+
+}
---
+++ Milestone2/Assets/Character/Controller/SwordController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwordController : MonoBehaviour {
+	public GameObject host_character;
+	private Animator animator;
+	private bool slashed = false;
+	// Use this for initialization
+	void Awake(){
+		this.animator = host_character.GetComponent<Animator> ();
+	}
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!this.animator.GetCurrentAnimatorStateInfo (0).IsName ("Slash")) {
+			this.slashed = false;
+		}
+	
+	}
+	void OnTriggerEnter(Collider other){
+		GameObject other_object = other.gameObject;
+		if (!(other_object == host_character) && other_object.layer == 9 && this.animator.GetCurrentAnimatorStateInfo(0).IsName("Slash") && other.GetType() == typeof(CharacterController)) {
+			CharacterHealth damage_script = other_object.GetComponent<CharacterHealth>();
+			if (!slashed) {
+				damage_script.SlashDamage ();
+				this.slashed = true;
+			}
+		}
+	}
+}
