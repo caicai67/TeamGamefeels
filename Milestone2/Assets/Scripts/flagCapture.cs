5897ae7f21a8c47b933d3de4a5041575220a61a2
caicai67 <caitlinmorris@Caitlins-MacBook-Pro.local>
2016-08-03 20:35:02 -0400

    added cutscene for flag captured

--- Milestone2/Assets/Scripts/flagCapture.cs
+++ Milestone2/Assets/Scripts/flagCapture.cs
@@ -5,6 +5,7 @@ public class flagCapture : MonoBehaviour {
 
 	public GameObject closedTower;
 	public GameObject openedTower;
+	public MovieTexture cutscene2;
 
 	// Use this for initialization
 	void Start () {
@@ -24,7 +25,15 @@ public class flagCapture : MonoBehaviour {
 		}
 	}
 	void openTower(){
+		if (cutscene2 != null) {
+			cutscene2.Play ();
+		}
 		openedTower.SetActive (true);
 		closedTower.SetActive (false);
+	}
+	void OnGUI() {
+		if (cutscene2 != null && cutscene2.isPlaying) {
+			GUI.DrawTexture (new Rect (0, 0, Screen.width, Screen.height), cutscene2);
+		}
 	}
 }
