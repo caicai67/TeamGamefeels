9a841c735e0fa72a73df5b3dad88c44d8437b989
Ambrose Cheung <cheungambrose30@gmail.com>
2016-08-03 20:53:27 -0400

    Silenced footsteps when sneaking

--- Milestone2/Assets/Character/Controller/PlayerFootsteps.cs
+++ Milestone2/Assets/Character/Controller/PlayerFootsteps.cs
@@ -17,45 +17,66 @@ public class PlayerFootsteps : MonoBehaviour {
 	public bool front_foot = true;
 	public bool step_triggered = false;
 	private float offset_timer = 0f;
-	// Use this for initialization
-	void Start () {
+    private Animator animator;
+    private GameObject character;
+    // Use this for initialization
+    void Start () {
 		this.audio.volume = 0.2f;
-	}
+        this.character = GameObject.FindGameObjectWithTag("Player");
+        this.animator = character.GetComponent<Animator>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		if (other_foot.step_triggered && this.offset_timer > this.time_offset) {
-			this.step_triggered = false;
-			this.offset_timer = 0f;
-		} else {
-			this.offset_timer += Time.deltaTime;
-		}
+        //if (animator.GetBool("sneak") == false)
+        //{
+            if (other_foot.step_triggered && this.offset_timer > this.time_offset)
+            {
+                this.step_triggered = false;
+                this.offset_timer = 0f;
+            }
+            else
+            {
+                this.offset_timer += Time.deltaTime;
+            }
+        //}
 	}
 
 	void OnTriggerEnter(Collider floor){
-		if (player.velocity.magnitude > 0.4f) {
-			if (floor.gameObject.layer == 10 && !this.step_triggered) {
-				audio.clip = this.metal_step;
-				this.offset_timer = 0f;
-				audio.Play ();
-				this.step_triggered = true;
-			} else if (floor.gameObject.layer == 11 && !this.step_triggered) {
-				audio.clip = this.concrete_step;
-				this.offset_timer = 0f;
+        if (animator.GetBool("sneak") == false)
+        {
+            if (player.velocity.magnitude > 0.4f)
+            {
+                if (floor.gameObject.layer == 10 && !this.step_triggered)
+                {
+                    audio.clip = this.metal_step;
+                    this.offset_timer = 0f;
+                    audio.Play();
+                    this.step_triggered = true;
+                }
+                else if (floor.gameObject.layer == 11 && !this.step_triggered)
+                {
+                    audio.clip = this.concrete_step;
+                    this.offset_timer = 0f;
 
-				audio.Play ();
-				this.step_triggered = true;
-			} else if (floor.gameObject.layer == 13 && !this.step_triggered) {
-				audio.clip = this.dirt_step;
-				audio.Play ();
-				this.offset_timer = 0f;
-				this.step_triggered = true;
-			} else if (floor.gameObject.layer == 15 && !this.step_triggered) {
-                audio.clip = this.wood_step;
-                audio.Play();
-                this.offset_timer = 0f;
-                this.step_triggered = true;
+                    audio.Play();
+                    this.step_triggered = true;
+                }
+                else if (floor.gameObject.layer == 13 && !this.step_triggered)
+                {
+                    audio.clip = this.dirt_step;
+                    audio.Play();
+                    this.offset_timer = 0f;
+                    this.step_triggered = true;
+                }
+                else if (floor.gameObject.layer == 15 && !this.step_triggered)
+                {
+                    audio.clip = this.wood_step;
+                    audio.Play();
+                    this.offset_timer = 0f;
+                    this.step_triggered = true;
+                }
             }
-    }
+        }
 	}
 }
